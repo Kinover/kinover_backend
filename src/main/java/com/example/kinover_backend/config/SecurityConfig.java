4fb1680606d084b4a040f6d53a76f459c7d75982
zzizi <zzi_zi@naver.com>
2025-04-02 16:50:14 +0900

    .

--- src/main/java/com/example/kinover_backend/config/SecurityConfig.java
+++ src/main/java/com/example/kinover_backend/config/SecurityConfig.java
@@ -51,8 +51,9 @@ public class SecurityConfig {
                 "http://localhost:8081",
                 "http://localhost:3000",
                 "http://localhost:3001",
-                "http://13.209.88.75:8081"
-        )); // ✅ 프론트 주소만 정확히 넣기
+                "http://13.124.192.206:8081"
+
+                )); // ✅ 프론트 주소만 정확히 넣기
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Collections.singletonList("*"));
         config.setAllowCredentials(true);
