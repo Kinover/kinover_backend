1bb272927be8a516a4f5e1399ed4a5f5e793964b
ChungJuwon <juwon1226@naver.com>
2025-05-03 21:48:08 +0000

    fix: add missing JpaRepository import at MessageRepository.java

--- src/main/java/com/example/kinover_backend/repository/MessageRepository.java
+++ src/main/java/com/example/kinover_backend/repository/MessageRepository.java
@@ -2,6 +2,7 @@ package com.example.kinover_backend.repository;
 
 import com.example.kinover_backend.entity.ChatRoom;
 import com.example.kinover_backend.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
